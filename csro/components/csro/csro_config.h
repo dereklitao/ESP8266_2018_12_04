3169afc0789fba339036a2d6aca044e61c18c2bf
dereklitao <dereklitao@outlook.com>
2018-12-05 17:03:53 +0800

    some change

--- csro/components/csro/csro_config.h
+++ csro/components/csro/csro_config.h
@@ -1,9 +1,21 @@
 #ifndef CSRO_CONFIG_H_
 #define CSRO_CONFIG_H_
 
-#include "stdint.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
 #include "MQTTClient.h"
-#include "FreeRTOS/MQTTFreeRTOS.h"
 
 
 #ifdef 	   DEBUG
